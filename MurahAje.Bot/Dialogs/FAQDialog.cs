519b1fb3c19c06e021043820e3245e984521e26b
mifmasterz <mifmasterz@yahoo.com>
2017-02-24 17:40:54 +0700

    update bot

---
+++ MurahAje.Bot/Dialogs/FAQDialog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Microsoft.Bot.Builder.Azure;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.CognitiveServices.QnAMaker;
+using System.Configuration;
+
+namespace MurahAje.Bot
+{
+    // For more information about this template visit http://aka.ms/azurebots-csharp-qnamaker
+    [Serializable]
+    public class FAQDialog : QnAMakerDialog
+    {
+        // Go to https://qnamaker.ai and feed data, train & publish your QnA Knowledgebase.
+        public FAQDialog() : base(new QnAMakerService(new QnAMakerAttribute(ConfigurationManager.AppSettings["QnASubscriptionKey"], ConfigurationManager.AppSettings["QnAKnowledgebaseId"])))
+        {
+        }
+    }
+}
